d119d4dd1ed1c115dd2e81fd218909418b7c6326
KaiArnhard <zadorikars@gmail.com>
2022-10-11 21:15:25 +0300

    +dump

--- main.cpp
+++ main.cpp
@@ -4,11 +4,19 @@
 
 typedef double elem_t;
 
+struct StackInfo
+{
+    elem_t *data;
+    size_t capacity;
+    size_t size;
+};
+
 struct Stack
 {
     elem_t *data;
     size_t capacity;
     size_t size;
+    StackInfo stDump;
 };
 
 const int poison = 'SOIP';
@@ -23,12 +31,14 @@ void StackDump(Stack *stack);
 
 int main()
 {
-    Stack st1 = { }; 
-    printf("%c", poison); 
+    Stack st1 = { };  
 
     StackInit(&st1, 6);
+    StackDump(&st1);
+    StackPush(&st1, 54);
+    StackDump(&st1);
     StackDtor(&st1);
-    
+
     return 0;
 }
 
@@ -36,12 +46,18 @@ void StackInit(Stack *stack, size_t capacity)
 {
     assert(capacity);
 
+    (stack->stDump).capacity = capacity;
     stack->capacity = capacity;
+    
     stack->data = (elem_t*) calloc(stack->capacity, sizeof(elem_t));
-    stack->size = 0;
+    assert(stack->data);
+    
+    (stack->stDump).data = stack->data;
+    (stack->stDump).size = stack->size = 0;
+    
     for (size_t counter = 0; counter < stack->capacity; counter++)
     {
-        stack->data[counter] = poison;
+        (stack->data)[counter] = poison;
     }
 }
 
@@ -59,6 +75,7 @@ void StackPop(Stack *stack, elem_t *elem)
     *(stack->data) = 0;
     stack->size--;
 }
+
 void StackDtor(Stack* stack)
 {
     stack->size  = NULL;
@@ -73,13 +90,32 @@ void StackDtor(Stack* stack)
 
 void StackDump(Stack *stack)
 {
-    Stack st2 = { };
-    st2.capacity = stack->capacity;
-    st2.size = stack->size;
-
-    for (size_t counter = 0; counter < st2.size; counter++)
+    if (stack->data != (elem_t*) poison)
     {
-        
+        printf("Number of stack elements %d\n", stack->size);
+        printf("Stack capacity %d\n", stack->capacity);
+        printf("Pointer to the first element of stack %p, pointer to the top of stack %p\n", (stack->stDump).data, stack->data);
+        printf("Stack elements:\n");
+
+        for (size_t counter = 0; counter < stack->capacity; counter++)
+        {
+            printf("%d\t%lg\n", counter, (stack->stDump).data[counter]);
+        }  
     }
-    
+    else if (stack->data == (elem_t*) poison)
+    {
+        printf("Stack was destroyed, values after destructions\n");
+        printf("Number of stack elements %d\n", stack->size);
+        printf("Stack capacity %d\n", stack->capacity);
+        printf("Pointer to the first element of stack %p, pointer to the top of stack %p\n", stack->stDump.data, stack->data);
+        printf("Stack elements:\n");
+
+        for (size_t counter = 0; counter < (stack->stDump).capacity; counter++)
+        {
+            printf("%d\t%lg\n", counter, (stack->stDump).data[counter]);
+        }
+    }
+    printf("\nClick enter button to continue\n");
+    while (!getchar())
+        { }       
 }
